cb6b80b9652fa20fd2ec21a60704f4ced95ea861
Enrico Secco <enricosecco@edu.unifil.br>
2025-03-04 00:24:35 -0300

    feat: update role and experience details in content.js; replace avatar image

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -6,7 +6,7 @@ const person = {
   get name() {
     return `${this.firstName} ${this.lastName}`;
   },
-  role: "Frontend Engineer",
+  role: "Frontend Software Engineer",
   avatar: "/images/avatar.jpg",
   location: "Brazil", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
   languages: ["Portuguese", "English"], // optional: Leave the array empty if you don't want to display languages
@@ -72,7 +72,7 @@ const about = {
     display: true,
   },
   calendar: {
-    display: true,
+    display: false,
     link: "https://cal.com",
   },
   intro: {
@@ -80,9 +80,7 @@ const about = {
     title: "Introduction",
     description: (
       <>
-        Selene is a Jakarta-based design engineer with a passion for transforming complex challenges
-        into simple, elegant design solutions. Her work spans digital interfaces, interactive
-        experiences, and the convergence of design and technology.
+        As a frontend developer with 5 years of experience, I build dynamic, user-centric web applications with JavaScript, TypeScript, React, and Next.js. I don’t just write code—I craft seamless digital experiences that solve real-world problems.
       </>
     ),
   },
@@ -91,41 +89,100 @@ const about = {
     title: "Work Experience",
     experiences: [
       {
-        company: "FLY",
-        timeframe: "2022 - Present",
-        role: "Senior Design Engineer",
+        company: "Wiley",
+        timeframe: "Aug 2023 - Present",
+        role: "Frontend Software Engineer",
         achievements: [
+          // <b>Frontend Development</b>,
           <>
-            Redesigned the UI/UX for the FLY platform, resulting in a 20% increase in user
-            engagement and 30% faster load times.
+            Led the transition to TypeScript, improving type safety, reducing runtime errors, and enhancing overall code maintainability, driving a more stable development environment.
           </>,
           <>
-            Spearheaded the integration of AI tools into design workflows, enabling designers to
-            iterate 50% faster.
+            Developed dynamic and responsive UIs using ReactJS and TypeScript, integrating seamlessly with REST APIs to deliver scalable and robust applications that improved user experience and performance.
           </>,
+          <>
+            Spearheaded the modernization of frontend architecture, removing deprecated packages and outdated build tools, ensuring long-term maintainability and smooth integration with evolving systems.
+          </>,
+          // <b>Testing & Quality Assurance</b>,
+          <>
+            Engineered and maintained unit tests with Jest and E2E tests with Cypress, ensuring high code quality and reliability. Refactored existing functions and applied Cypress best practices, reducing E2E test execution time from 1 hour to 25 minutes, and contributing to a 20% increase in test coverage.
+          </>,
+          <>
+            Documented and standardized React components using Storybook, facilitating team collaboration and ensuring consistent implementation across the frontend.
+          </>,
+          // <b>Collaboration & Knowledge Sharing</b>
+          <>
+            Collaborated with backend developers in an API-first approach, ensuring seamless integration and alignment between frontend and backend services, resulting in a more efficient development proces
+          </>,
+          <>
+            Mentored Quality Engineers (QEs) in a Dojo-style project, guiding them through Cypress testing and best practices, improving their ability to write and maintain tests, enhancing overall team capability.
+          </>,
+          <>
+            Introduced code reviews and knowledge-sharing sessions, fostering a culture of continuous improvement and ensuring the team was up to date with the latest technologies and best practices.
+          </>,
+          <>
+            Configured and documented projects to run in Docker, streamlining development and deployment processes, improving consistency across environments, and accelerating development cycles with efficient setup using make commands.
+          </>,
+          <>
+            Established and enforced coding standards and best practices, ensuring the codebase remained maintainable, scalable, and aligned with the project's long-term goals.
+          </>,
+          <>
+            Actively participated as a developer within a Scrum team, adhering to Agile principles, engaging in daily stand-ups, refinement sessions, and retrospectives, which ensured continuous feedback and iterative improvement within the project.
+          </>
+
         ],
         images: [
           // optional: leave the array empty if you don't want to display images
-          {
-            src: "/images/projects/project-01/cover-01.jpg",
-            alt: "Once UI Project",
-            width: 16,
-            height: 9,
-          },
+          // {
+          //   src: "/images/projects/project-01/cover-01.jpg",
+          //   alt: "Once UI Project",
+          //   width: 16,
+          //   height: 9,
+          // },
         ],
       },
       {
-        company: "Creativ3",
-        timeframe: "2018 - 2022",
-        role: "Lead Designer",
+        company: "Solus Saúde",
+        timeframe: "Jan 2021 - Aug 2023",
+        role: "Fullstack Software Engineer",
         achievements: [
+          // <b>Frontend Development</b>,
           <>
-            Developed a design system that unified the brand across multiple platforms, improving
-            design consistency by 40%.
+            Led the complete overhaul of a legacy web application by migrating it to React and TypeScript, driving a 30% increase in client sales, a 40% reduction in bug reports, and a 60% decrease in support tickets.
           </>,
           <>
-            Led a cross-functional team to launch a new product line, contributing to a 15% increase
-            in overall company revenue.
+            Architected high-performance frontend solutions based on design system, by creating reusable components using React Hook Form, Axios, and Redux, significantly improving code maintainability and efficiency. By introducing reusable components, the development cycle time reduced 25%, enabling faster feature delivery.
+          </>,
+          <>
+            Transformed the online sales experience by leading the development of a scalable web-based system for online health plan sales, leading to a 15% increase in sales.
+          </>,
+          <>
+            Led the creation of a commercial web platform that optimized sales workflows and prospects for health plan representatives, improving usability and performance, and driving a 20% increase in conversions.
+          </>,
+          <>
+            Enhanced platform security by implementing a two-factor authentication system, boosting user trust and compliance with security standards.
+          </>,
+
+          // <b>Backend Development</b>,
+          <>
+            Developed and maintained Delphi REST APIs using the MVC pattern, and maintained
+            legacy Node.js APIs, ensuring seamless integration and functionality during the transition to new systems.
+          </>,
+          <>
+            Refined critical backend processes by refactoring a major reimbursement functionality, resolving performance bottlenecks.
+          </>,
+          <>
+            Integrated a credit card payment processing solution, reducing payment-related issues by 30%, and ensuring secure and efficient transaction handling.
+          </>,
+
+          // <b>DevOps & Infrastructure</b>,
+          <>
+            Engineered and managed Linux-based development and production environments using Docker and Nginx, streamlining deployment pipelines and ensuring robust application performance.
+          </>,
+
+          // <b>Leadership & Collaboration</b>,
+          <>
+            Participated in resolving complex bugs and optimizing workflows, conducting targeted training for support teams to enable faster issue resolution and increase operational efficiency.
           </>,
         ],
         images: [],
@@ -137,13 +194,10 @@ const about = {
     title: "Studies",
     institutions: [
       {
-        name: "University of Jakarta",
-        description: <>Studied software engineering.</>,
-      },
-      {
-        name: "Build the Future",
-        description: <>Studied online marketing and personal branding.</>,
-      },
+        name: "Unifil - Centro Universitário Filadélfia",
+        timeframe: "2019 - 2023",
+        description: <>Software Engineering Bachelor</>,
+      }
     ],
   },
   technical: {
@@ -155,18 +209,18 @@ const about = {
         description: <>Able to prototype in Figma with Once UI with unnatural speed.</>,
         // optional: leave the array empty if you don't want to display images
         images: [
-          {
-            src: "/images/projects/project-01/cover-02.jpg",
-            alt: "Project image",
-            width: 16,
-            height: 9,
-          },
-          {
-            src: "/images/projects/project-01/cover-03.jpg",
-            alt: "Project image",
-            width: 16,
-            height: 9,
-          },
+          // {
+          //   src: "/images/projects/project-01/cover-02.jpg",
+          //   alt: "Project image",
+          //   width: 16,
+          //   height: 9,
+          // },
+          // {
+          //   src: "/images/projects/project-01/cover-03.jpg",
+          //   alt: "Project image",
+          //   width: 16,
+          //   height: 9,
+          // },
         ],
       },
       {
@@ -174,12 +228,12 @@ const about = {
         description: <>Building next gen apps with Next.js + Once UI + Supabase.</>,
         // optional: leave the array empty if you don't want to display images
         images: [
-          {
-            src: "/images/projects/project-01/cover-04.jpg",
-            alt: "Project image",
-            width: 16,
-            height: 9,
-          },
+          // {
+          //   src: "/images/projects/project-01/cover-04.jpg",
+          //   alt: "Project image",
+          //   width: 16,
+          //   height: 9,
+          // },
         ],
       },
     ],
